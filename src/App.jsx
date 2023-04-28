10ad05e7f5751a68e91d129a10fbad62f47f8680
Pawel Konior <pawel.konior@gmail.com>
2023-04-28 10:13:22 +0200

    all functionalities without components composition and advanced abstraction

--- src/App.jsx
+++ src/App.jsx
@@ -1,132 +1,197 @@
-import './App.css'
-import {useEffect, useState} from "react";
-import {deleteDataAPI, getDataAPI, sendDataAPI} from "./helpers/api.js";
+import "./App.css";
+import { useEffect, useState } from "react";
+import {
+  deleteDataAPI,
+  getDataAPI,
+  sendDataAPI,
+  updateDataAPI,
+} from "./helpers/api.js";
 import AddOperation from "./components/AddOperation.jsx";
 import AddTimeSpent from "./components/AddTimeSpent.jsx";
 
 function App() {
-    const [tasks, setTasks] = useState([]);
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [operationId, setOperationId] = useState(null);
-    const [timeSpentId, setTimeSpentId] = useState(null);
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [operationId, setOperationId] = useState(null);
+  const [timeSpentId, setTimeSpentId] = useState(null);
 
-    useEffect(() => {
-        const data = Promise.all([getDataAPI('tasks'), getDataAPI('operations')])
+  useEffect(() => {
+    const data = Promise.all([getDataAPI("tasks"), getDataAPI("operations")]);
 
-        data
-            .then((results) => {
-                const [taskData, operationData] = results;
-                const tasks = taskData.map((task) => ({
-                    ...task,
-                    operations: operationData.filter((operation) => operation.taskId === task.id)
-                }))
+    data
+      .then((results) => {
+        const [taskData, operationData] = results;
+        const tasks = taskData.map((task) => ({
+          ...task,
+          operations: operationData.filter(
+            (operation) => operation.taskId === task.id
+          ),
+        }));
 
-                setTasks(tasks);
-            })
-            .catch(console.error)
+        setTasks(tasks);
+      })
+      .catch(console.error);
+  }, []);
 
-    }, [])
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const result = await sendDataAPI(
+      {
+        title,
+        description,
+        status: "open",
+        addedDate: new Date(),
+      },
+      "tasks"
+    );
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const result = await sendDataAPI({
-            title, description, status: 'open', addedDate: new Date()
-        }, 'tasks');
+    setTitle("");
+    setDescription("");
+    setTasks([...tasks, result]);
+  }
 
-        setTitle('');
-        setDescription('');
-        setTasks([...tasks, result])
+  async function handleDeleteTask(event) {
+    const id = +event.target.dataset.id;
+    const task = tasks.find((task) => task.id === id);
+
+    for (const operation of task.operations) {
+      await deleteDataAPI(operation.id, "operations");
     }
 
-    async function handleDeleteTask(event) {
-        const id = +event.target.dataset.id
-        await deleteDataAPI(id, 'tasks');
-        setTasks(tasks.filter((task) => task.id !== id));
-    }
+    await deleteDataAPI(id, "tasks");
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
 
-    async function handleDeleteOperation(id) {
-        await deleteDataAPI(id, 'operations');
-        setTasks(tasks.map((task) => {
-            return {
-                ...task,
-                operations: task.operations.filter((operation) => operation.id !== id)
-            }
+  async function handleDeleteOperation(id) {
+    await deleteDataAPI(id, "operations");
+    setTasks(
+      tasks.map((task) => {
+        return {
+          ...task,
+          operations: task.operations.filter(
+            (operation) => operation.id !== id
+          ),
+        };
+      })
+    );
+  }
+
+  function handleFinishTask(id) {
+    return async function () {
+      await updateDataAPI(
+        {
+          status: "closed",
+        },
+        "tasks",
+        id,
+        "PATCH"
+      );
+
+      setTasks(
+        tasks.map((task) => ({
+          ...task,
+          status: task.id === id ? "closed" : task.status,
         }))
-    }
+      );
+    };
+  }
 
-    return (
-        <>
-            <form
-                onSubmit={handleSubmit}
-            >
-                <div>
-                    <label htmlFor="title">Title</label>
-                    <input
-                        value={title}
-                        type="text"
-                        id="title"
-                        name="title"
-                        onChange={(event) => setTitle(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="desc">Description</label>
-                    <textarea
-                        value={description}
-                        id="desc"
-                        name="desc"
-                        onChange={(event) => setDescription(event.target.value)}
-                    />
-                </div>
-                <button type="submit">Add</button>
-            </form>
-            <br/>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            value={title}
+            type="text"
+            id="title"
+            name="title"
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="desc">Description</label>
+          <textarea
+            value={description}
+            id="desc"
+            name="desc"
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+      <br />
 
-            <section>
-                {tasks.map((task) => (
-                    <div key={task.id}>
-                        <b>{task.title}</b> - <span>{task.description}</span>
-                        {operationId === task.id ? (
-                            <AddOperation
-                                setOperationId={setOperationId}
-                                taskId={task.id}
-                                setTasks={setTasks}
-                            />
-                        ) : (
-                            <button onClick={() => setOperationId(task.id)}>
-                                Add operation
-                            </button>
+      <section>
+        {tasks.map((task) => (
+          <div key={task.id}>
+            <b>{task.title}</b> - <span>{task.description}</span>
+            {operationId === task.id ? (
+              <AddOperation
+                setOperationId={setOperationId}
+                taskId={task.id}
+                setTasks={setTasks}
+              />
+            ) : (
+              <>
+                {task.status === "open" && (
+                  <button onClick={() => setOperationId(task.id)}>
+                    Add operation
+                  </button>
+                )}
+              </>
+            )}
+            {task.status === "open" && (
+              <button onClick={handleFinishTask(task.id)}>Finish</button>
+            )}
+            <button onClick={handleDeleteTask} data-id={task.id}>
+              Delete
+            </button>
+            <div>
+              {task.operations &&
+                task.operations.map((operation) => (
+                  <div key={operation.id}>
+                    <span>{operation.description}: </span>
+                    {operation.timeSpent !== 0 && (
+                      <b>
+                        {~~(operation.timeSpent / 60)}h{" "}
+                        {operation.timeSpent % 60}m
+                      </b>
+                    )}
+
+                    {operation.id === timeSpentId ? (
+                      <AddTimeSpent
+                        setTasks={setTasks}
+                        operationId={operation.id}
+                        timeSpent={operation.timeSpent}
+                        setTimeSpentId={setTimeSpentId}
+                      />
+                    ) : (
+                      <>
+                        {task.status === "open" && (
+                          <button onClick={() => setTimeSpentId(operation.id)}>
+                            Add Spent Time
+                          </button>
                         )}
-                        <button>Finish</button>
-                        <button onClick={handleDeleteTask} data-id={task.id}>Delete</button>
+                      </>
+                    )}
 
-                        <div>
-                            {task.operations && task.operations.map((operation) => (
-                                <div key={operation.id}>
-                                    <span>{operation.description}: </span><b>{~~(operation.timeSpent / 60)}h {operation.timeSpent % 60}m</b>
-                                    {operation.id === timeSpentId ? (
-                                        <AddTimeSpent
-                                            setTasks={setTasks}
-                                            operationId={operation.id}
-                                            timeSpent={operation.timeSpent}
-                                            setTimeSpentId={setTimeSpentId}
-                                        />
-                                    ) : (
-                                        <button onClick={() => setTimeSpentId(operation.id)}>Add Spent Time</button>
-                                    )}
-                                    <button
-                                        onClick={() => handleDeleteOperation(operation.id)}
-                                    >Delete
-                                    </button>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
+                    {task.status === "open" && (
+                      <button
+                        onClick={() => handleDeleteOperation(operation.id)}
+                      >
+                        Delete
+                      </button>
+                    )}
+                  </div>
                 ))}
-            </section>
-        </>
-    )
+            </div>
+          </div>
+        ))}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/AddTimeSpent.jsx
+++ src/components/AddTimeSpent.jsx
@@ -1,49 +1,55 @@
-import {useState} from "react";
-import {updateDataAPI} from "../helpers/api.js";
+import { useState } from "react";
+import { updateDataAPI } from "../helpers/api.js";
 import PropTypes from "prop-types";
 
-function AddTimeSpent({operationId, timeSpent, setTasks, setTimeSpentId}) {
-    const [value, setValue] = useState(0)
+function AddTimeSpent({ operationId, timeSpent, setTasks, setTimeSpentId }) {
+  const [value, setValue] = useState(0);
 
-    async function handleUpdateOperation() {
-        await updateDataAPI(
-            {"timeSpent": value + timeSpent},
-            'operations',
-            operationId,
-            'PATCH'
-        )
+  async function handleUpdateOperation() {
+    if (value > 0) {
+      await updateDataAPI(
+        { timeSpent: value + timeSpent },
+        "operations",
+        operationId,
+        "PATCH"
+      );
 
-        setTasks((prev) => prev.map((task) => ({
-            ...task,
-            operations: task.operations.map((operation) => {
-                if (operation.id === operationId) {
-                    operation.timeSpent += value
-                }
-                return operation
-            })
-        })));
+      setTasks((prev) =>
+        prev.map((task) => ({
+          ...task,
+          operations: task.operations.map((operation) => {
+            if (operation.id === operationId) {
+              operation.timeSpent += value;
+            }
+            return operation;
+          }),
+        }))
+      );
 
-        setTimeSpentId(null);
+      setTimeSpentId(null);
     }
+  }
 
-    return (
-        <>
-            <input
-                value={value}
-                onChange={(e) => setValue(e.target.valueAsNumber)}
-                type="number"
-            />
-            <button onClick={handleUpdateOperation}>Add</button>
-            <button onClick={() => setTimeSpentId(null)}>Cancel</button>
-        </>
-    );
+  return (
+    <>
+      {value < 0 && <b>Wartość musi być dodatnia</b>}
+      <input
+        value={value}
+        onChange={(e) => setValue(+e.target.value)}
+        type="number"
+        min="0"
+      />
+      <button onClick={handleUpdateOperation}>Add</button>
+      <button onClick={() => setTimeSpentId(null)}>Cancel</button>
+    </>
+  );
 }
 
 AddTimeSpent.propTypes = {
-    operationId: PropTypes.number,
-    timeSpent: PropTypes.number,
-    setTasks: PropTypes.func,
-    setTimeSpentId: PropTypes.func
-}
+  operationId: PropTypes.number,
+  timeSpent: PropTypes.number,
+  setTasks: PropTypes.func,
+  setTimeSpentId: PropTypes.func,
+};
 
 export default AddTimeSpent;
